3c9d3e07c1277bdc4976ff6e4b64f5249a66f00e
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2023-04-11 06:03:44 +0200

    third

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="bisection"
+          href="newton"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Bisection{' '}
+            Newton{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
